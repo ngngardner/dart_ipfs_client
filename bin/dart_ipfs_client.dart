1b58e7296e9ea3f539c564b50867127adb559fbc
Noah Gardner <ngngardner@gmail.com>
2021-03-27 18:04:14 -0400

    first commit

---
+++ bin/dart_ipfs_client.dart
@@ -0,0 +1,5 @@
+import 'package:dart_ipfs_client/dart_ipfs_client.dart' as dart_ipfs_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_ipfs_client.calculate()}!');
+}
---
+++ test/dart_ipfs_client_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_ipfs_client/dart_ipfs_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
